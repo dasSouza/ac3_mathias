bb739d546783eda42de87a6e8e9f9289847f2db5
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-04-02 19:44:38 -0300

    feat: Implementação do form na tela de login

--- SiteInstitucional/projeto-site/public/js/js.js
+++ SiteInstitucional/projeto-site/public/js/js.js
@@ -74,3 +74,52 @@ function closeModal(mn) {
     document.body.style.overflow = 'auto';
 }
 
+// FUNCÕES LOGIN
+function entrar() {
+    aguardar();
+    var formulario = new URLSearchParams(new FormData(form_login));
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        body: formulario
+    }).then(resposta => {
+
+        if (resposta.ok) {
+
+            resposta.json().then(json => {
+
+                sessionStorage.login_usuario_meuapp = json.login;
+                sessionStorage.nome_usuario_meuapp = json.nome;
+
+                if (json.administrador == 1) {
+                    window.location.href = 'dashboard.html';
+                } else {
+                    window.location.href = 'dashboard_usuario.html';
+                }
+            });
+
+        } else {
+
+            console.log('Erro de login!');
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                finalizar_aguardar(texto);
+            });
+        }
+    });
+
+    return false;
+}
+
+function aguardar() {
+    btn_entrar.disabled = true;
+    img_aguarde.style.visibility = 'visible';
+    div_erro.style.visibility = 'hidden';
+}
+
+function finalizar_aguardar(resposta) {
+    btn_entrar.disabled = false;
+    img_aguarde.style.visibility = 'hidden';
+    div_erro.style.visibility = 'visible';
+    div_erro.innerHTML = resposta;
+}
