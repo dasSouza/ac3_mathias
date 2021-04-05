402b5119f51e62d7c0990bfbb149ea7eb77d569f
loisfg <luis.oliveira@bandtec.com.br>
2021-04-05 08:26:14 -0300

    feat: cria pagina tutorial, js com timer de finalização, css com estilos da pagina
    tutorial está incompleto

--- SiteInstitucional/projeto-site/public/js/dash.js
+++ SiteInstitucional/projeto-site/public/js/dash.js
@@ -11,6 +11,42 @@ document.querySelector('.toggle').onclick = function () {
     navigation.classList.toggle('active');
 }
 
+function darkMode() {
+    var element = document.querySelector('main');
+    element.classList.toggle("dark-mode");
+ }
+
+/* teste */
+
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 10, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="demo"
+  demo.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('demo').innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 next.addEventListener('click', function() {
     idesContainer.style.display = 'none';
     idesContainerPart2.style.display = 'flex';
@@ -20,9 +56,3 @@ back.addEventListener('click', function() {
     idesContainerPart2.style.display = 'none';
     idesContainer.style.display = 'flex';
 });
-
-function darkMode() {
-    var element = document.querySelector('main');
-    element.classList.toggle("dark-mode");
- }
-
