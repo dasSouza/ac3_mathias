3c65788511fe3f07a81d9ebbc52be2135e92b822
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2021-04-14 18:09:44 -0300

    feat: dados sendo gerados no grafico da tela graficosHardware.html a partir do banco

--- SiteInstitucional/projeto-site/routes/leituras.js
+++ SiteInstitucional/projeto-site/routes/leituras.js
@@ -4,17 +4,20 @@ var sequelize = require('../models').sequelize;
 var Leitura = require('../models').Leitura;
 
 /* Recuperar as últimas N leituras */
-router.get('/ultimas/:idcaminhao', function (req, res, next) {
+router.get('/ultimas/:idprocessos', function (req, res, next) {
 
 	// quantas são as últimas leituras que quer? 8 está bom?
 	const limite_linhas = 1;
 
-	var idcaminhao = req.params.idcaminhao;
+	var idprocessos = req.params.idprocessos;
 
 	console.log(`Recuperando as ultimas ${limite_linhas} leituras`);
 
 	const instrucaoSql = `select top ${limite_linhas} 
-						us_ide_ram
+						us_dt_hr_end_IDE,
+						us_ide_ram,
+						us_ide_cpu,
+						us_ide_disco
 						from tb_processos_ide
 						order by id_processos desc`;
 						
@@ -24,6 +27,7 @@ router.get('/ultimas/:idcaminhao', function (req, res, next) {
 	})
 		.then(resultado => {
 			console.log(`Encontrados: ${resultado.length}`);
+			console.log(resultado)
 			res.json(resultado);
 		}).catch(erro => {
 			console.error(erro);
@@ -31,13 +35,13 @@ router.get('/ultimas/:idcaminhao', function (req, res, next) {
 		});
 });
 
-router.get('/tempo-real/:idcaminhao', function (req, res, next) {
+router.get('/tempo-real/:idprocessos', function (req, res, next) {
 	console.log('Recuperando caminhões');
 
-	//var idcaminhao = req.body.idcaminhao; // depois de .body, use o nome (name) do campo em seu formulário de login
-	var idcaminhao = req.params.idcaminhao;
+	//var idprocessos = req.body.idprocessos; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var idprocessos = req.params.idprocessos;
 
-	let instrucaoSql = `select top 2 us_dt_hr_processo, us_ide_total_processo from tb_processos_ide where fk_id_ide = ${idcaminhao} order by id_processos desc`;
+	let instrucaoSql = `select top 2 us_dt_hr_processo, us_ide_total_processo from tb_processos_ide where fk_id_ide = ${idprocessos} order by id_processos desc`;
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
