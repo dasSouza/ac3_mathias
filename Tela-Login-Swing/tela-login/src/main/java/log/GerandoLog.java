f04db6e3fe1d215e6e0b1805f7e721369f7f2567
BrunoRicardoGomesdaSilva <bruno.silva@bandtec.com.br>
2021-05-16 15:12:25 -0300

    add: resolvido o problema do log, agora ele funciona normal

--- Tela-Login-Swing/tela-login/src/main/java/log/GerandoLog.java
+++ Tela-Login-Swing/tela-login/src/main/java/log/GerandoLog.java
@@ -1,20 +1,26 @@
 package log;
 
-    import java.io.FileWriter ;
-    import java.io.IOException ;
-    import java.io.PrintWriter ;
-    import java.util.Scanner ;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class GerandoLog {
-    
-    public void gravarLog(String textoLog) throws IOException{
-        FileWriter arq = new FileWriter("C:\\Users\\mathias.de.carvalho\\Desktop\\Git\\KeepCode-Grupo-08\\log.txt");
+
+    Date dataHoraAtual = new Date();
+    String data = new SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
+    String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+
+    public void gravarLog(String textoLog) throws IOException {
+        FileWriter arq = new FileWriter("C:\\Users\\DOM\\Documents\\Bandtec Grupo 8 KeepCode\\KeepCode-Grupo-08\\log.txt", true);
         PrintWriter gravarArq = new PrintWriter(arq);
 
         gravarArq.println(textoLog);
-        
+
         arq.close();
-    
+
     }
 
 }
