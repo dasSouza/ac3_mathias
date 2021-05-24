c419b68f6c9a8fe4f6a0bceedb517c65756df948
PiresIsabella <isabella.silva@valemobi.com.br>
2021-05-24 16:13:35 -0300

    Add: Altera API para teste de ligacao com docker

--- ProcessosAPI/src/main/java/AppKeepCode/KeepCodeAPI.java
+++ ProcessosAPI/src/main/java/AppKeepCode/KeepCodeAPI.java
@@ -6,28 +6,27 @@ import ProcessoMaq.GetAllProcess;
 import ProcessosIDE.ProcessIDE;
 
 public class KeepCodeAPI {
+
     public static void main(String[] args) {
         GetAllProcess getAllDatesProcess = new GetAllProcess();
         ProcessIDE processosIDE = new ProcessIDE();
 
-//        getAllDatesProcess.getNamePc();
+        getAllDatesProcess.getNamePc();
         getAllDatesProcess.getDiscoTotal();
-//        getAllDatesProcess.getCpuNome();
-//        getAllDatesProcess.memoriaTotal();
-//
-//        getAllDatesProcess.insertDatesMaquina();
+        getAllDatesProcess.getCpuNome();
+        getAllDatesProcess.memoriaTotal();
+//        getAllDatesProcess.insertDatesMaquina();;
 
-//        processosIDE.putAllNameIde();
-//        processosIDE.getIdeName();
-//        processosIDE.getIdeCpu();
-//        processosIDE.getIdeRam();
-//        processosIDE.getIdeDisco();
-//        processosIDE.getRamValues();
+        processosIDE.putAllNameIde();
+        processosIDE.getIdeName();
+        processosIDE.getIdeCpu();
+        processosIDE.getIdeRam();
+        processosIDE.getIdeDisco();
+        processosIDE.getRamValues();
 
-//        processosIDE.showAll();
+        processosIDE.showAll();
 //
 //        processosIDE.insertIntoValues();
 
     }
 }
-
--- ProcessosAPI/src/main/java/DAO/ProcessIdeDAO.java
+++ ProcessosAPI/src/main/java/DAO/ProcessIdeDAO.java
@@ -20,7 +20,7 @@ public class ProcessIdeDAO {
             Double ram = allIdeDates.getValoresRamIDE().get(i);
             Float cpu = allIdeDates.getValoresCpuIDE().get(i);
 
-            String insertProcessValues = "INSERT INTO tb_processos_ide (us_dt_hr_start_IDE, us_dt_hr_end_IDE, us_ide_nome_processo, us_ide_ram, us_ide_cpu, us_ide_disco) VALUES (GETDATE(), GETDATE(), ? , ? , ?, ?)";
+            String insertProcessValues = "INSERT INTO tb_processos_ide (us_dt_hr_start_IDE, us_dt_hr_end_IDE, us_ide_nome_processo, us_ide_ram, us_ide_cpu, us_ide_disco, fk_id_maquina) VALUES (GETDATE(), GETDATE(), ? , ? , ?, ?, 2)";
             template.update(insertProcessValues, nomeIDE,ram, cpu, disco);
             System.out.println("inserindo no banco: " + "nome da IDE: " + nomeIDE + "\nRam: " + ram + "\nCPU:" + cpu + "\nDisco: " + disco);
         }
