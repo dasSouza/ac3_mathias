e55b671744436d9a1beb663455f7d0bce7dd33a8
PiresIsabella <isabella.silva@valemobi.com.br>
2021-05-25 16:54:43 -0300

    Feat: Testando conexao com banco de dados

--- ProcessosAPI/src/main/java/ConectionBDA/Conection.java
+++ ProcessosAPI/src/main/java/ConectionBDA/Conection.java
@@ -5,14 +5,14 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class Conection {
     private BasicDataSource dataSource;
 
-        public Conection() {
+       public Conection() {
             this.dataSource = new BasicDataSource();
-            dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            dataSource.setUrl("jdbc:sqlserver://grupo-8.database.windows.net;" +
-                    "databaseName=kcode;");
-            dataSource.setUsername("grupo-8");
-            dataSource.setPassword("#Gfkcode8");
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306");
+            dataSource.setUsername("root");
+            dataSource.setPassword("admin");
         }
+
 
         public BasicDataSource getDataSource() {
             return dataSource;
