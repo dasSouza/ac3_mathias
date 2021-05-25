d01f8cfcb549fe61da5b6a22ad0078735f4cfd9a
PiresIsabella <isabella.silva@valemobi.com.br>
2021-05-25 19:39:20 -0300

    Feat: Altera insert de ProcessIde para valor chumbado

--- ProcessosAPI/src/main/java/DAO/ProcessMaqDAO.java
+++ ProcessosAPI/src/main/java/DAO/ProcessMaqDAO.java
@@ -15,7 +15,7 @@ public class ProcessMaqDAO {
     JdbcTemplate template = new JdbcTemplate(conection.getDataSource());
 
     public void maquinaProcess(MaquinaDatas maquinaDatas) {
-        String insertProcessValues = "INSERT INTO tb_us_maquina(us_nome_maquina, us_vl_ram_total, us_vl_disco_total ,us_vl_cpu_total) VALUES (?, ?, ?, ?)";
+        String insertProcessValues = "INSERT INTO tb_us_maquina(us_nome_maquina, us_vl_ram_total, us_vl_disco_total ,us_vl_cpu_total, fk_id_funcionario) VALUES (?, ?, ?, ?, 69449167052)";
         template.update(insertProcessValues,maquinaDatas.getUs_name_pc(), maquinaDatas.getUs_ram_total().toString(), maquinaDatas.getUs_disco_total().toString(), maquinaDatas.getUs_cpu_nome());
         System.out.println("Inserindo dados no banco de dados: " + maquinaDatas.toString());
 
