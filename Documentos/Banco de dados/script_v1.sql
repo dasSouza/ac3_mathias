16e7f5341088c06ed2a31f67739bd7ca8b1e133b
gustavo.oliveira <gustavo.oliveira@valemobi.com.br>
2021-05-26 23:44:21 -0300

    fix: corrige erro de syntax

--- Documentos/Banco de dados/script_v1.sql
+++ Documentos/Banco de dados/script_v1.sql
@@ -1,32 +1,22 @@
-Dash gestor -> gestor sabendo qual maquina está acessando
-
 SELECT * FROM tb_us_maquina AS maq
 JOIN tb_us_dados AS us
 ON maq.fk_id_funcionario = us.id_cpf;
 
-
-Dash dev -> buscando RAM e nome da IDE
 SELECT DISTINCT us_ide_ram, fk_id_maquina, us_ide_nome_processo
 FROM tb_processos_ide AS ide
 	JOIN tb_us_maquina AS maq
 	ON ide.fk_id_maquina = 1;
 
-
-exemplo de cód: 
 SELECT DISTINCT us_ide_ram, fk_id_maquina, us_ide_nome_processo
 FROM tb_processos_ide AS ide
 	JOIN tb_us_maquina AS maq
 	ON ide.fk_id_maquina = (id_maq);
-
-buscando todos os dados da IDE:
 
 SELECT DISTINCT us_ide_ram, us_ide_disco, us_ide_cpu, us_dt_hr_start_IDE, us_dt_hr_end_IDE, us_ide_nome_processo,fk_id_maquina
 FROM tb_processos_ide AS ide
 	JOIN tb_us_maquina AS maq
 	ON ide.fk_id_maquina = (id_maq);
 
-
-Para saber de qual empresa é aquela equipe: 
 SELECT DISTINCT us_nome_funcionario, us_equipe, fk_id_empresa
 FROM tb_us_dados AS usuario
 	JOIN tb_us_maquina AS maq
