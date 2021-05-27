a886a366ca6548151b69eb3020515abda8e0cc03
BrunoRicardoGomesdaSilva <bruno.silva@bandtec.com.br>
2021-05-27 01:33:22 -0300

    add: scripts e comandos uteis, e script da ac3 de SO versão 2

---
+++ Documentos/Scripts3/KeepCodeApp.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+RED='\\033[0;31m'
+NC='\\033[0m' # No Color
+VERSAO=11
+
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá Aluno, serei seu assistente para instalação do Java!;"
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Verificando aqui se você possui o Java instalado...;"
+
+java -version
+if [ $? -eq 0 ]; then
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá você já tem o java instalado!!"
+else
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Confirme para mim se realmente deseja instalar o Java (S/N)?"
+	read inst
+	if [ \"$inst\" == \"s\" ]; then
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Ok! Você escolheu instalar o Java ;D"
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Adicionando o repositório!"
+		sleep 2
+		sudo add-apt-repository ppa:webupd8team/java -y
+		clear
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Atualizando! Quase lá."
+		sleep 2
+		sudo apt update -y
+		clear
+
+		if [ $VERSAO -eq 11 ]; then
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 11 do Java. Confirme a instalação quando solicitado ;D"
+			sudo apt install default-jre
+			apt install openjdk-11-jre-headless
+			-y
+			clear
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
+		fi
+
+	else
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Você optou por não instalar o Java por enquanto, até a próxima então!"
+	fi
+fi
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Verificando aqui se você possui o Docker instalado...;"
+docker --version
+if [ $? -eq 0 ]; then
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá você já tem o Docker instalado!!"
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Iniciando o docker!!"
+	sleep 2
+	sudo systemctl start docker
+	sudo systemctl enable docker
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Docker iniciado com sucesso"
+	sudo docker run -d -p 3306:3306 --name ConteinerBD -e "MYSQL_DATABASE=banco1" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+	sudo docker exec -it ConteinerBD bash
+else
+
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Você não tem o docker instalado, deseja instalar agora (S/N)?"
+	read inst
+	if [ \"$inst\" == \"s\" ]; then
+		mkdir -p ./KeepCode/docker
+		cd KeepCode/appKcode
+		sudo apt install docker.io
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) A aplicação foi baixada com sucesso"
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Obrigado por baixar o docker"
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) O docker será iniciado, deseja iniciar agora (S/N)?"
+		read inst
+		if [ \"$inst\" == \"s\" ]; then
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Vamos iniciar o Docker e entrar no mysql"
+			sleep 2
+			sudo systemctl start docker
+			sudo systemctl enable docker
+			sudo docker run -d -p 3306:3306 --name ConteinerBD -e "MYSQL_DATABASE=banco1" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Docker iniciado com sucesso"
+			sudo docker exec -it ConteinerBD bash
+		else
+			echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Você optou por não iniciar o docker"
+			exit
+		fi
+	else
+		echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Você optou por não instalar o docker"
+		exit
+	fi
+
+fi
+
+# ===================================================================
+# Todos direitos reservados para o autor: Dra. Profa. Marise Miranda.
+# Sob licença Creative Commons @2020
+# Podera modificar e reproduzir para uso pessoal.
+# Proibida a comercialização e a exclusão da autoria.
+# ===================================================================
