b85142104d4ad7135cabb7490b21f3e647638ee6
loisfg <luis.oliveira@bandtec.com.br>
2021-05-26 21:01:16 -0300

    sql

--- ProcessosAPI/src/main/java/AppKeepCode/KeepCodeAPI.java
+++ ProcessosAPI/src/main/java/AppKeepCode/KeepCodeAPI.java
@@ -5,28 +5,36 @@ import DAO.UsuariosDAO;
 import ProcessoMaq.GetAllProcess;
 import ProcessosIDE.ProcessIDE;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class KeepCodeAPI {
     public static void main(String[] args) {
         GetAllProcess getAllDatesProcess = new GetAllProcess();
-        ProcessIDE processosIDE = new ProcessIDE();
 
         getAllDatesProcess.getNamePc();
         getAllDatesProcess.getDiscoTotal();
         getAllDatesProcess.getCpuNome();
         getAllDatesProcess.memoriaTotal();
-
         getAllDatesProcess.insertDatesMaquina();
 
-        processosIDE.putAllNameIde();
-        processosIDE.getIdeName();
-        processosIDE.getIdeCpu();
-        processosIDE.getIdeRam();
-        processosIDE. getIdeDisco();
+        int delay = 1000;   // tempo de espera antes da 1ª execução da tarefa.
+        int interval = 7000;  // intervalo no qual a tarefa será executada.
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                ProcessIDE processosIDE = new ProcessIDE();
+                processosIDE.putAllNameIde();
+                processosIDE.getIdeName();
+                processosIDE.getIdeCpu();
+                processosIDE.getIdeRam();
+                processosIDE.getIdeDisco();
 
-//        processosIDE.showAll();
+                processosIDE.showAll();
 
-        processosIDE.insertIntoValues();
-
+                processosIDE.insertIntoValues();
+            }
+        }, delay, interval);
     }
 }
 
--- ProcessosAPI/src/main/java/DAO/ProcessIdeDAO.java
+++ ProcessosAPI/src/main/java/DAO/ProcessIdeDAO.java
@@ -20,17 +20,18 @@ public class ProcessIdeDAO {
             Float cpu = allIdeDates.getValoresCpuIDE().get(i);
 
             String insertProcessValues = "INSERT INTO tb_processos_ide (us_dt_hr_start_IDE, us_dt_hr_end_IDE, us_ide_nome_processo, us_ide_ram, us_ide_cpu, us_ide_disco, fk_id_maquina) VALUES (GETDATE(), GETDATE(), ? , ? , ?, ?, 1)";
+            String insertProcessValues2 = "INSERT INTO tb_processos_ide (us_dt_hr_start_IDE, us_dt_hr_end_IDE, us_ide_nome_processo, us_ide_ram, us_ide_cpu, us_ide_disco) VALUES (NOW(), NOW(), ? , ? , ?, ?)";
             template.update(insertProcessValues, nomeIDE,ram, cpu, disco);
+            template2.update(insertProcessValues2, nomeIDE,ram, cpu, disco);
             System.out.println("inserindo no banco: " + "nome da IDE: " + nomeIDE + "\nRam: " + ram + "\nCPU:" + cpu + "\nDisco: " + disco);
         }
     }
     
     public void createTable() {
-        template2.execute("IF NOT EXISTS" +
-                "CREATE DATABASE banco1;\n" +
-                "USE banco1;\n" +
+        template2.execute(
+                "USE IF NOT EXISTS banco1;\n" +
                 "\n" +
-                "CREATE TABLE tb_us_maquina (\n" +
+                "CREATE TABLE IF NOT EXISTS tb_us_maquina (\n" +
                 "  id_maquina BIGINT NOT NULL auto_increment primary key,\n" +
                 "  us_nome_maquina VARCHAR(100) NOT NULL,\n" +
                 "  us_vl_ram_total FLOAT NULL,\n" +
@@ -38,7 +39,7 @@ public class ProcessIdeDAO {
                 "  us_vl_cpu_total VARCHAR(50) NULL\n" +
                 ");\n" +
                 "\n" +
-                "CREATE TABLE tb_processos_ide (\n" +
+                "CREATE TABLE IF NOT EXISTS tb_processos_ide (\n" +
                 "  id_processos BIGINT NOT NULL auto_increment primary key,\n" +
                 "  us_dt_hr_start_IDE DATETIME NULL,\n" +
                 "  us_dt_hr_end_IDE DATETIME NULL,\n" +
