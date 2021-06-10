a32da59d6d3961b688f56376da69918d098ec627
BrunoRicardoGomesdaSilva <bruno.silva@bandtec.com.br>
2021-06-10 19:07:45 -0300

    add2

--- ProcessosAPI/src/main/java/DAO/ProcessMaqDAO.java
+++ ProcessosAPI/src/main/java/DAO/ProcessMaqDAO.java
@@ -12,16 +12,10 @@ import java.time.LocalTime;
 
 public class ProcessMaqDAO {
 
-    Conection conection = new Conection();
-    JdbcTemplate template = new JdbcTemplate(conection.getDataSource());
     DockerConnection conDocker = new DockerConnection();
     JdbcTemplate template2 = new JdbcTemplate(conDocker.getBanco());
 
     public void maquinaProcess(MaquinaDatas maquinaDatas) {
-        String insertProcessValues = "INSERT INTO tb_us_maquina(us_nome_maquina, us_vl_ram_total, us_vl_disco_total ,us_vl_cpu_total, fk_id_funcionario) VALUES (?, ?, ?, ?)";
-        template.update(insertProcessValues, maquinaDatas.getUs_name_pc(), maquinaDatas.getUs_ram_total().toString(), maquinaDatas.getUs_disco_total().toString(), maquinaDatas.getUs_cpu_nome());
-        System.out.println("Inserindo dados no banco de dados: " + maquinaDatas.toString());
-
         String insertProcessValues2 = "INSERT INTO tb_us_maquina(us_nome_maquina, us_vl_ram_total, us_vl_disco_total ,us_vl_cpu_total) VALUES (?, ?, ?, ?)";
         template2.update(insertProcessValues2, maquinaDatas.getUs_name_pc(), maquinaDatas.getUs_ram_total().toString(), maquinaDatas.getUs_disco_total().toString(), maquinaDatas.getUs_cpu_nome());
         System.out.println("Inserindo dados no banco de dados: " + maquinaDatas.toString());
